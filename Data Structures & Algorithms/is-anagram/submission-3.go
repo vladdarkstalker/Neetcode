77050112ba908624781043d20bb035d06c733f55
Владислав Заволженский <119122181+vladdarkstalker@users.noreply.github.com>
2026-06-15 10:28:54 +0300

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.go
@@ -0,0 +1,21 @@
+func isAnagram(s string, t string) bool {
+    var lenS = len(s)
+    if lenS != len(t) {
+        return false
+    }
+
+    var bufferS = make(map[rune]int)
+    var bufferT = make(map[rune]int)
+    for i:=0; i<lenS; i++ {
+        bufferS[rune(s[i])]+=1
+        bufferT[rune(t[i])]+=1
+    }
+
+    for _, el := range t {
+        if bufferS[el] != bufferT[el] {
+            return false
+        }
+    }
+
+    return true
+}
